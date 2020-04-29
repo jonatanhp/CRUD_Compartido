2de0cb3c85a8647ddddf5828e612be966fcd7b1f
jonatanhp <jonatan.hp@upeu.edu.pe>
2020-04-29 12:57:17 -0500

    primer commit desde eclipse

---
+++ src/crud_compartido/Principal.java
@@ -0,0 +1,10 @@
+package crud_compartido;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("principal desde eclipse para compartir");
+	}
+
+}
